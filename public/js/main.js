85dab186f268e5d0ae4ed93ed04d1f066e8f964b
Peter <peter@ursem.cc>
2023-04-17 12:32:15 -0600

    color change test

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,10 @@ doc.rect(0.5,0.0625,1,0.125,'F');
 
 //Document watermark
 doc.setFontSize(12);
-doc.text("Peter's Cut-Erator", 0.25, 1, {'angle': 270});
+doc.text("Peter's Cut-Erator", 0.25, 4, {'angle': 270});
 
 //Add business card (with bleed)
+doc.setDrawColor(15,100,100,0);
 doc.rect(0.375,0.1875,3.75,2.125,'F');
 
 doc.save('test.pdf');
