c1c9b22895fcd63eb2dabb42391ee0c5ffafaa13
Peter <peter@ursem.cc>
2023-04-17 12:42:02 -0600

    Draw cards with variables

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,18 @@
+const documentFormats = {
+    'business_card': {
+        'margins': {
+            'x':0.375,
+            'y':0.1875,
+            'gutter': 0.25
+        },
+        'images': {
+            'width': 3.75,
+            'height': 2.125
+        },
+        'layout': [2,5]
+    }
+}
+
 const doc = new jspdf.jsPDF({
     orientation: "portrait",
     unit: "in",
@@ -13,6 +28,6 @@ doc.text("Peter's Cut-Erator", 0.25, 4, {'angle': 270});
 
 //Add business card (with bleed)
 doc.setDrawColor(15,100,100,0);
-doc.rect(0.375,0.1875,3.75,2.125,'F');
+doc.rect(documentFormats.business_card.margins.x,documentFormats.business_card.margins.y,documentFormats.business_card.images.width,documentFormats.business_card.images.height,'F');
 
 doc.save('test.pdf');
