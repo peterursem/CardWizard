63e82de49c6a2b58935cf9072b7abc163766cf42
Peter <peter@ursem.cc>
2023-04-17 12:28:45 -0600

    using rect instead of rectangle

--- public/js/main.js
+++ public/js/main.js
@@ -5,13 +5,13 @@ const doc = new jspdf.jsPDF({
 });
 
 //Make calibration strip
-doc.rectangle(0.5,0.0625,1,0.125);
+doc.rect(0.5,0.0625,1,0.125,'F');
 
 //Document watermark
 doc.setFontSize(12);
 doc.text("Peter's Cut-Erator", 0.25, 1, {'angle': 270});
 
 //Add business card (with bleed)
-doc.rectangle(0.375,0.1875,3.75,2.125);
+doc.rect(0.375,0.1875,3.75,2.125,'F');
 
 doc.save('test.pdf');
