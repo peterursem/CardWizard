0e99129a538ff1ef5766370a2461fc4d9e4b5899
Peter <peter@ursem.cc>
2023-04-17 11:50:00 -0600

    Testing document creation and importing modules

---
+++ public/js/main.mjs
@@ -0,0 +1,12 @@
+import { jsPDF } from './node_modules/jspdf/dist/jspdf.es';
+
+const doc = new jsPDF({
+    orientation: "portrait",
+    unit: "in",
+    format: [8.5, 11]
+});
+
+doc.setFontSize(12);
+doc.text("Peter's Cut-Erator", 1, 1, {'angle': 270});
+
+doc.save('test.pdf');
